33f46dced564d4e1257abfc2bd8dccc91d12b91c
chuhuy <45164683+chuhuy@users.noreply.github.com>
2020-12-07 15:46:26 +0700

    Disable redis

--- src/graphql/explore/controllers/chart.js
+++ src/graphql/explore/controllers/chart.js
@@ -1,7 +1,4 @@
 const Explore = require("./../models/explore");
-const redis = require("redis");
-const PORT_REDIS = process.env.PORT || 6379;
-const client = redis.createClient(PORT_REDIS);
 
 const chartController = (args) => {
   let explore = new Explore();
